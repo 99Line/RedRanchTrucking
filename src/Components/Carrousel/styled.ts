aecba4f36e3f227fa1c45c247906c0817a8ddc0a
erickdominguez <erickdomroli@gmail.com>
2023-09-27 15:07:31 -0500

    new styles and fixed heaeder

---
+++ src/Components/Carrousel/styled.ts
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled'
+import { Box } from '@mui/material'
+import {theme} from 'Theme/theme'
+export const ContainerBox = styled(Box)`
+    background: theme.; height: '70%';
+`
