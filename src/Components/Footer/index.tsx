fd1d6def053269a56c3f0859cb04984cad7a81b2
EstebanXP <cuc1800@gmail.com>
2024-01-10 21:11:28 -0600

    mini fix

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -1,49 +1,49 @@
-import React from "react";
-import { DataContainer, FooterContainer, Line, RedStripe } from "./styled";
-import LocalPhoneOutlinedIcon from "@mui/icons-material/LocalPhoneOutlined";
-import CopyrightOutlinedIcon from "@mui/icons-material/CopyrightOutlined";
-import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
-import PlaceOutlinedIcon from "@mui/icons-material/PlaceOutlined";
-import { Typography } from "@mui/material";
-import { theme } from "Theme/theme";
-import { info } from "Utils/strings";
-import { useMobile } from "Utils/mixins";
+import React from 'react'
+import { DataContainer, FooterContainer, Line, RedStripe } from './styled'
+import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined'
+import CopyrightOutlinedIcon from '@mui/icons-material/CopyrightOutlined'
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined'
+import PlaceOutlinedIcon from '@mui/icons-material/PlaceOutlined'
+import { Typography } from '@mui/material'
+import { theme } from 'Theme/theme'
+import { info } from 'Utils/strings'
+import { useMobile } from 'Utils/mixins'
 
-const { ADDRESS, CELPHONE, EMAIL } = info;
+const { ADDRESS, CELPHONE, EMAIL } = info
 
 const Footer = () => {
-  const mobile = useMobile();
+  const mobile = useMobile()
 
   return (
     <FooterContainer>
       <DataContainer
         style={{
-          padding: mobile ? "2rem 0px 0px 2rem" : "3.625rem 0px 0px 5rem",
+          padding: mobile ? '2rem 0px 0px 2rem' : '3.625rem 0px 0px 5rem',
         }}
       >
         <Typography variant="h3" color={theme.extraBackgrounds.burgundyColor}>
           Red Ranch Trucking LLC
         </Typography>
         <Line>
-          <CopyrightOutlinedIcon style={{ color: "white", fontSize: "20px" }} />
+          <CopyrightOutlinedIcon style={{ color: 'white', fontSize: '20px' }} />
           <Typography
             variant="body1"
             color={theme.extraBackgrounds.grayColor}
-            sx={{ fontSize: "20px", fontWeight: "600" }}
+            sx={{ fontSize: '20px', fontWeight: '600' }}
           >
             Red Ranch Trucking LLC
           </Typography>
         </Line>
         <Line>
           <LocalPhoneOutlinedIcon
-            style={{ color: "white", fontSize: "20px" }}
+            style={{ color: 'white', fontSize: '20px' }}
           />
 
-          <a href={`tel:${EMAIL}`} style={{ textDecoration: "none" }}>
+          <a href={`tel:${CELPHONE}`} style={{ textDecoration: 'none' }}>
             <Typography
               variant="body1"
               color={theme.extraBackgrounds.grayColor}
-              sx={{ fontSize: "20px", fontWeight: "600" }}
+              sx={{ fontSize: '20px', fontWeight: '600' }}
             >
               {/*TODO Add links */}
               {CELPHONE}
@@ -51,14 +51,14 @@ const Footer = () => {
           </a>
         </Line>
         <Line>
-          <EmailOutlinedIcon style={{ color: "white", fontSize: "20px" }} />
+          <EmailOutlinedIcon style={{ color: 'white', fontSize: '20px' }} />
           <a href={`mailto:${EMAIL}`}>
             <Typography
               variant="body1"
               sx={{
-                textDecoration: "underline",
-                fontSize: "20px",
-                fontWeight: "600",
+                textDecoration: 'underline',
+                fontSize: '20px',
+                fontWeight: '600',
               }}
               color={theme.extraBackgrounds.grayColor}
             >
@@ -68,11 +68,11 @@ const Footer = () => {
         </Line>
         <Line>
           {/*TODO Add links */}
-          <PlaceOutlinedIcon style={{ color: "white", fontSize: "20px" }} />
+          <PlaceOutlinedIcon style={{ color: 'white', fontSize: '20px' }} />
           <Typography
             variant="body1"
             color={theme.extraBackgrounds.grayColor}
-            sx={{ fontSize: "20px", fontWeight: "600" }}
+            sx={{ fontSize: '20px', fontWeight: '600' }}
           >
             {ADDRESS}
           </Typography>
@@ -80,7 +80,7 @@ const Footer = () => {
       </DataContainer>
       <RedStripe></RedStripe>
     </FooterContainer>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
